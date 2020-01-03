261266ca0e8c10d9bf9bee23718ba4b36f4597c5
okutamatanuki <seinoazumi@gmail.com>
2020-01-03 20:53:35 +0900

    implement devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'informations#index'
   get '/about', to: 'informations#about'
 
+  resources :users
   resources :works, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
