bf27bfec060482057a03c607c7da4d80beb046b6
okutamatanuki <seinoazumi@gmail.com>
2020-01-05 09:56:27 +0900

    set enum for pulldown list

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,11 @@
 class Work < ApplicationRecord
   mount_uploader :image, ImageUploader
+
+  enum category: {
+    default: 0,
+    programing: 1,
+    web_design: 2,
+    dtp_design: 3,
+    other: 4
+  }, _prefix: true
 end
