e8c48c2150ab9110075d3d925b5342b71c4672dd
okutamatanuki <seinoazumi@gmail.com>
2020-01-06 19:19:44 +0900

    add some column and validation

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,7 +1,22 @@
 class Work < ApplicationRecord
+  # モデルのバリデーション
+  with_options presence: true do
+    validates :category
+    validates :date
+    validates :description
+    validates :images
+    validates :name
+    validates :time
+  end 
+
+  # カスタムバリデーション
+  validation :images_should_be_more_than_two
+
+  # アップローダーの設定
   mount_uploaders :images, ImageUploader
   serialize :images, JSON
 
+  # enumの設定
   enum category: {
     default: 0,
     programing: 1,
@@ -9,4 +24,11 @@ class Work < ApplicationRecord
     dtp_design: 3,
     other: 4
   }, _prefix: true
+
+  # カスタムバリデーションのメソッド
+  def images_should_be_more_than_two
+    if images.present? && images.length < 2
+      errors.add(:images, ": 画像は２つ以上投稿してください")
+    end
+  end
 end
---
+++ db/migrate/20200106100514_add_url_and_slider_to_works.rb
@@ -0,0 +1,6 @@
+class AddUrlAndSliderToWorks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :works, :url, :string
+    add_column :works, :recomend, :boolean, default: false
+  end
+end
