604704dae6b45c798bfedae3d88f36d47c03768d
Azumi Seino <49363119+seinoazumi@users.noreply.github.com>
2020-01-06 19:22:51 +0900

    Revert "add some column and validation"

--- db/migrate/20200106100514_add_url_and_slider_to_works.rb
+++
@@ -1,6 +0,0 @@
-class AddUrlAndSliderToWorks < ActiveRecord::Migration[5.2]
-  def change
-    add_column :works, :url, :string
-    add_column :works, :recomend, :boolean, default: false
-  end
-end
