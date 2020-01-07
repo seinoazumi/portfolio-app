10ee0f8e51f65c22979e9b674befb90af80164fc
okutamatanuki <seinoazumi@gmail.com>
2020-01-07 14:44:35 +0900

    fix the gallery.js

--- app/assets/javascripts/gallery.js
+++ app/assets/javascripts/gallery.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
-  $(whindow).load(function(){
+  $(window).load(function(){
+
     var $container = $('#works');
     $container.imagesLoaded( function() {
       $container.masonry({ //mansonryの対象を#worksにする
         itemSelector: '.works__img',
-        columnWidth: '.col', 
-        isFitWidth: true  //親要素の幅に合わせてカラム数を自動調整
+        columnWidth: '.img-fluid',
+        gutter: 0
       });  
     });
   });
+
 });
