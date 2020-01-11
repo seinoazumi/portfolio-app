3ed958c5eded58ea589fdec8b71d66a6a7e63c3e
okutamatanuki <seinoazumi@gmail.com>
2020-01-12 08:36:36 +0900

    add controllar and add validation to model

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,2 +1,6 @@
 class Inquiry < ApplicationRecord
+  validates :name, presence: true, length: { maximum:255 }
+  validates :email, presence: true, length: { maximum:255 },
+                    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :message, presence: true
 end
