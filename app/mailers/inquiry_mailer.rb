30e9849cac6f932f4d4ad5f5956b7cb36a97cec3
okutamatanuki <seinoazumi@gmail.com>
2020-01-12 08:54:46 +0900

    remove email

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,7 +2,7 @@ class InquiryMailer < ApplicationMailer
   def send_mail(inquiry)
     @inquiry = inquiry
     mail(from: "system@no-reply.com",
-         to: "seinoazumi@gmail.com",
+         to: ENV['MAIL'],
          subject: "お問い合わせ通知")
   end
 end
