ad8cf926d308c2bb2353aec38d6b802809cc57f8
okutamatanuki <seinoazumi@gmail.com>
2020-01-12 09:53:43 +0900

    add environment variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,8 +64,8 @@ config.action_mailer.smtp_settings = {
  address:              'smtp.gmail.com',
  port:                  587,
  domain:               'gmail.com',
- user_name:            '<gmailのメールアドレス>',
- password:             '<gmailのパスワード>',
+ user_name:            ENV['MAIL'],
+ password:             ENV['MAIL_PW'],
  authentication:       'plain',
  enable_starttls_auto:  true
 }
