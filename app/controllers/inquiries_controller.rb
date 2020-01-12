2b19917d7e3b8cae8d8a4092695fce65af3a9a47
okutamatanuki <seinoazumi@gmail.com>
2020-01-12 09:22:09 +0900

    build inquiry form

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,2 +1,16 @@
 class InquiriesController < ApplicationController
+  def creare
+    @inquiry = Inquiry.new(inquiry_params)
+    if @inquiry.save
+      flash[:success] = "お問い合わせを受け付けました"
+      redirect_to root_path
+    else
+      render root_path
+    end
+  end
+
+private
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :email, :message)
+  end
 end
