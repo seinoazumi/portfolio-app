f9a8291eca72042ab7473fa2dab7286396a8b8c3
okutamatanuki <seinoazumi@gmail.com>
2020-01-20 17:19:43 +0900

    redirect test

--- spec/controllers/sign_up_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe RegistrationsController do
-  it "ログインしていない状態でsign_upページに行くとリダイレくとされる" do
-    visit new_user_registration_path
-    expect(response).to redirect_to root_path
-  end
-end
