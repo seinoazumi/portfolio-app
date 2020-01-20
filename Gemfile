8e89ea080ac9ad00032a3d8bc4c83f3105230b4f
okutamatanuki <seinoazumi@gmail.com>
2020-01-20 13:37:32 +0900

    build information test works test

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
+  gem 'factory_bot_rails', '~> 4.11'
 end
 
 group :development do
@@ -48,7 +49,6 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  gem 'factory_bot_rails', '~> 4.11'
 end
 
 group :test do
---
+++ spec/system/works_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe 'Works 作品一覧', type: :system do
+  describe '作品一覧表示ページ' do
+
+    
+    let(:login_user) { FactoryBot.create(:other_user) }
+
+    
+    before do
+      visit new_user_session_path
+      fill_in 'Email', with: login_user.email
+      fill_in 'Password', with: login_user.password
+      click_on 'Log in'
+      visit root_path
+    #   work = FactoryBot.create(:work)
+    end
+
+    context '管理者としてログインしている時' do
+      let(:login_user) { FactoryBot.create(:admin_user) }
+
+      it '作品一覧ページに作品投稿ページのリンクが表示される' do
+        visit works_path
+        expect(page).to have_content '作品投稿ページへ'
+      end
+    end
+
+    context '管理者以外でログインしている時' do
+      let(:login_user) { FactoryBot.create(:other_user) }
+
+      it '作品一覧ページに作品投稿ページのリンクが表示される' do
+        visit works_path
+        expect(page).not_to have_content '作品投稿ページへ'
+      end
+    end
+    
+    it '作品一覧ページに行くと作品が表示される' do
+      # visit works_path
+      # expect(page).to have_selector 'img.works__img'
+      
+    end
+  end
+end
