0945ced0b528e29939ca090d148396822ae31b38
okutamatanuki <seinoazumi@gmail.com>
2020-01-26 17:43:27 +0900

    modify_index_view

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -1,7 +1,7 @@
 class InformationsController < ApplicationController
 
   def index
-    @works = Work.where(category: "programing").order(date: "DESC")
+    @works = Work.limit(4).order(date: "DESC")
     @inquiry = Inquiry.new
   end
 
