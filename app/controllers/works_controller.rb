f8f999a2199f3d434649b455d740a51f75956e69
okutamatanuki <seinoazumi@gmail.com>
2020-03-08 21:29:37 +0900

    improve label

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,8 +2,9 @@ class WorksController < ApplicationController
 
   def index
     @categories = Category.all
+    @works = Work.all if params.has_key?(:all_category)
+    @works = params.has_key?(:category) ?  Work.find(params[:category][:id]) : Work.all
     
-    @works = params.include?("commit") ?  Work.find(params[:category][:id]) : Work.all
     # @works = params[:category_id].present? ? Category.find(params[:category_id]).works : Work.all
   end
 
